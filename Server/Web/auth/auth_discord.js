0fea41f42921e510da6b526080be70117a4a6791
SkyLightQP <combbm@naver.com>
2018-12-04 21:24:28 +0900

    끄투닷컴 1.0.2.1

---
+++ Server/Web/auth/auth_discord.js
@@ -0,0 +1,34 @@
+const config = require('../../sub/auth.json');
+
+module.exports.config = {
+    strategy: require('passport-discord').Strategy,
+    color: '#7289DA',
+    fontColor: '#FFFFFF',
+    vendor: 'discord',
+    displayName: 'withDiscord'
+}
+
+module.exports.strategyConfig = {
+    clientID: config.discord.clientID, // 보안을 위해서입니다.
+    clientSecret: config.discord.clientSecret, // 이 방법을 사용하는 것을
+    callbackURL: config.discord.callbackURL, // 적극 권장합니다.
+    passReqToCallback: true
+}
+
+module.exports.authConfig = {
+    scope: ['identify', 'email']
+}
+
+module.exports.strategy = (process, MainDB, Ajae) => {
+    return (req, accessToken, refreshToken, profile, done) => {
+        const $p = {};
+
+        $p.authType = "discord";
+        $p.id = profile.id;
+        $p.name = profile.username;
+        $p.title = profile.username;
+        $p.image = "https://cdn.discordapp.com/avatars/" + profile.id + "/" + profile.avatar + ".jpg";
+
+        process(req, accessToken, MainDB, $p, done);
+    }
+}
---
+++ server-setup.bat
@@ -0,0 +1,7 @@
+echo Node.js Electron
+cd ./Server
+node setup
+echo KKutu Main Source
+cd ./lib
+echo npm start > ../run.bat
+grunt default pack
