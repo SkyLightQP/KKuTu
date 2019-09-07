ceb2c3639f2a94f5992bf669073fa9d1d4d53be1
SkyLightQP <combbm@naver.com>
2019-09-07 18:30:31 +0900

    fix: ban.js 오류 수정

--- Server/sub/ban.js
+++ Server/sub/ban.js
@@ -1,7 +1,7 @@
 const JLog	 = require("./jjlog");
 const MainDB = require('../Web/db');
 
-exports.checkIPBan = function (addr) {
+function checkIPBan(addr) {
     MainDB.ipbans.findOne(['ip', addr]).on((data) => {
         if (data !== undefined) {
             return true;
@@ -9,10 +9,13 @@ exports.checkIPBan = function (addr) {
     });
 };
 
-exports.noticeBan = function (res, addr, html) {
+function noticeBan(res, addr, html) {
     if(checkIPBan(addr)) {
         const reason = data.reason !== null ? data.reason : "사유 없음";
         JLog.alert("IP 차단된 사용자가 입장을 시도하였습니다. IP: " + addr);
         res.send("[#444] 당신은 영구 정지된 사용자입니다. <br/> 사유: " + reason);
     } else res.send(html);
 };
+
+exports.checkIPBan = checkIPBan;
+exports.noticeBan = noticeBan;
