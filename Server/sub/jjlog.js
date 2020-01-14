767f23825f8d5b12225203417272fe1e88b60fcd
SkyLightQP <combbm@naver.com>
2020-01-14 20:11:40 +0900

    chore: remove legacy code
    - fix: change log file path

--- Server/sub/jjlog.js
+++ Server/sub/jjlog.js
@@ -26,25 +26,6 @@ exports.init = function() {
 	logger.level = 'ALL';
 };
 
-// legacy code
-// function callLog(text){
-// 	var date = new Date();
-// 	var o = {
-// 		year: 1900 + date.getYear(),
-// 		month: date.getMonth() + 1,
-// 		date: date.getDate(),
-// 		hour: date.getHours(),
-// 		minute: date.getMinutes(),
-// 		second: date.getSeconds()
-// 	}, i;
-//
-// 	for(i in o){
-// 		if(o[i] < 10) o[i] = "0"+o[i];
-// 		else o[i] = o[i].toString();
-// 	}
-// 	console.log("["+o.year+"-"+o.month+"-"+o.date+" "+o.hour+":"+o.minute+":"+o.second+"] "+text);
-// }
-
 exports.log = function(text){
 	logger.info(text);
 };
