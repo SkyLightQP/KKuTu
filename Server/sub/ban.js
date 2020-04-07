366b6c57ec0355f76a68b7662c8bf5ec2ec9c0d9
SkyLightQP <combbm@naver.com>
2020-04-07 11:06:15 +0900

    fix: IP밴 오류 수정

--- Server/sub/ban.js
+++ Server/sub/ban.js
@@ -1,21 +1,16 @@
-const JLog	 = require("./jjlog");
+const JLog = require("./jjlog");
 const MainDB = require('../Web/db');
 
-function checkIPBan(addr) {
+function noticeBan(res, addr, html) {
     MainDB.ipbans.findOne(['ip', addr]).on((data) => {
         if (data !== undefined) {
-            return true;
-        } else return false;
+            const reason = data.reason !== null ? data.reason : "사유 없음";
+            JLog.alert("IP 차단된 사용자가 입장을 시도하였습니다. IP: " + addr);
+            res.send("[#444] 당신은 영구 정지된 사용자입니다. <br/> 사유: " + reason);
+        } else {
+            res.send(html);
+        }
     });
 };
 
-function noticeBan(res, addr, html) {
-    if(checkIPBan(addr)) {
-        const reason = data.reason !== null ? data.reason : "사유 없음";
-        JLog.alert("IP 차단된 사용자가 입장을 시도하였습니다. IP: " + addr);
-        res.send("[#444] 당신은 영구 정지된 사용자입니다. <br/> 사유: " + reason);
-    } else res.send(html);
-};
-
-exports.checkIPBan = checkIPBan;
 exports.noticeBan = noticeBan;
